a94fb4a43ac0392255c4df66a7e648a4cf7ba426
Fallfromgrace18 <149462981+Fallfromgrace18@users.noreply.github.com>
2024-06-03 13:00:57 -0400

    completed

--- index.js
+++ index.js
@@ -28,13 +28,15 @@ const nestedMuppet = {
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
-
+const[moo,neigh,baa,oink,cluck]=farmAnimals.split(" ")
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
-
+const[bessie,, dolly,babe,little]=farmAnimals.split(" ")
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
-
+const[blackAndWhite,,black,pink,,]=farmAnimals.split(" ")
 // Arrays
-
+//const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red,orange,yellow,green,blue,indigo,violet]=colors
+const [r,o,y,g,b,i,v]=colors
 // 4. Use destructuring to assign appropriate variables using the color names.
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
@@ -46,3 +48,40 @@ const nestedMuppet = {
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+
+const[,,,,,indg,]=colors
+
+
+// const muppet = {
+//   muppetName: 'Miss Piggy',
+//   color: 'pink',
+//   song: 'Never Before, Never Again',
+//   job: 'Cast member of The Muppet Show',
+//   partner: 'Kermit'
+// };
+
+// const nestedMuppet = {
+//   nestedName: 'Kermit',
+//   nestedColor: 'green',
+//   album: {
+//     theMuppetMovie: {
+//       song1: 'Rainbow Connection',
+//       song2: 'Moving Right Along',
+//       song3: 'Never Before, Never Again',
+//       song4: 'I Hope That Something Better Comes Along',
+//     },
+//   },
+//   nestedJob: 'Host of The Muppet Show',
+//   nestedPartner: 'Miss Piggy'
+// };
+const{muppetName}=muppet
+const{color}=muppet
+const{song}=muppet
+const{job}=muppet
+const{partner}=muppet
+//const{muppetName,color,song,job,partner}=muppet
+const{song2}=nestedMuppet.album.theMuppetMovie
+const{song4}=nestedMuppet.album.theMuppetMovie
+const{nestedJob}=nestedMuppet
+const{nestedPartner}=nestedMuppet
+
